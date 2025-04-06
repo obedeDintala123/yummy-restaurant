a5a62d285391609b2bd543b769d1bf030bc3ec94
obedeDintala123 <obededintala@gmail.com>
2025-04-06 12:45:02 +0200

    update app-sidebar.tsx

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -14,11 +14,11 @@ export const AppSidebar = ( { closeSidebar }: AppSidebarProps ) => {
         { name: "Login", href: "/login" },
     ]
     return (
-        <div className="fixed top-0 right-0 w-1/3 h-screen bg-[#38363F] z-50 flex flex-col gap-2">
+        <div className="fixed top-0 right-0 w-1/2 h-screen bg-[#38363F] z-50 flex flex-col gap-2">
             <div className="flex items-center justify-between w-full p-4 lg:px-10">
                 <h1 className="text-white text-xl font-medium">Yummy<span className="text-[#CF1113]">.</span></h1>
 
-                <X color="#fff" width={15} onClick={() => closeSidebar()} className="cursor-pointer"/>
+                <X color="#fff" width={20} onClick={() => closeSidebar()} className="cursor-pointer"/>
             </div>
             <nav className="flex flex-col gap-8 text-white font-light p-4">
                 {Links.map((link, index) => (
